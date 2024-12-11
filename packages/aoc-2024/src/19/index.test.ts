cb4b592eb49fce9de83b199d9948599e978477d1
Tim Schmelter <schmelte@amazon.com>
2024-12-11 14:01:57 -0800

    chore: clean up remaining test files

--- packages/aoc-2024/src/19/index.test.ts
+++ packages/aoc-2024/src/19/index.test.ts
@@ -1,32 +1,28 @@
 import { readToString } from '@palpatim/aoc-utils';
 import * as path from "node:path";
 
-const solve1 = (input: string): number => {
-  return -1;
-};
-
-const solve2 = (input: string): number => {
+const solve = (input: string): number => {
   return -1;
 };
 
 describe("aoc", () => {
   test("demo 1", () => {
     const input = readToString(path.join(__dirname, "input-demo.txt"));
-    expect(solve1(input)).toEqual(-1);
+    expect(solve(input)).toEqual(-1);
   });
 
   test("part 1", () => {
     const input = readToString(path.join(__dirname, "input.txt"));
-    expect(solve1(input)).toEqual(-1);
+    expect(solve(input)).toEqual(-1);
   });
 
   test("demo 2", () => {
     const input = readToString(path.join(__dirname, "input-demo.txt"));
-    expect(solve2(input)).toEqual(-1);
+    expect(solve(input)).toEqual(-1);
   });
 
   test("part 2", () => {
     const input = readToString(path.join(__dirname, "input.txt"));
-    expect(solve2(input)).toEqual(-1);
+    expect(solve(input)).toEqual(-1);
   });
 });
