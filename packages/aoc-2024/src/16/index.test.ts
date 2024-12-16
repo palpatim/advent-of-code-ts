d45e8d6d7e4f92d240d201c41ef2eb85e94d2374
Tim Schmelter <schmelte@amazon.com>
2024-12-16 14:22:27 -0800

    Day 16 WIP: part 2 still not working

--- packages/aoc-2024/src/16/index.test.ts
+++ packages/aoc-2024/src/16/index.test.ts
@@ -7,8 +7,10 @@ import {
   keyToPoint,
   NamedOffset,
   Point,
+  pointEq,
   pointToKey,
   readToString,
+  union,
 } from "@palpatim/aoc-utils";
 import * as path from "node:path";
 
@@ -48,7 +50,7 @@ const fromKey = (k: string): [Point, NamedOffset] => {
   return [keyToPoint(p), o as NamedOffset];
 };
 
-const solve = (input: string): number => {
+const solve = (input: string, returnCost: boolean = true): number => {
   const grid = input.split("\n").map((row) => row.split("") as GridItem[]);
 
   // Build adjacency lists. Key is a point and a direction, as in:
@@ -72,17 +74,16 @@ const solve = (input: string): number => {
 
     // Only consider adjacent cells in the same direction we're facing. Add that
     // cell plus a vertex for a rotation left, plus a rotation right. (We don't
-    // need to consider 180 degrees since we just came from there)
-    const leftKey = toKey(currentPoint, rotateLeft(currentFacingDirection));
-    if (!visited.has(leftKey)) {
-      stack.push(leftKey);
-      dijkstra.addEdge(currentKey, leftKey, 1000);
-    }
-
-    const rightKey = toKey(currentPoint, rotateRight(currentFacingDirection));
-    if (!visited.has(rightKey)) {
-      stack.push(rightKey);
-      dijkstra.addEdge(currentKey, rightKey, 1000);
+    // need to consider 180 degrees since we just came from there). As an
+    // optimization, we'll only consider a direction if it leads to a valid
+    // cell.
+    for (const newDirection of [
+      rotateLeft(currentFacingDirection),
+      rotateRight(currentFacingDirection),
+    ]) {
+      const newDirectionKey = toKey(currentPoint, newDirection);
+      stack.push(newDirectionKey);
+      dijkstra.addEdge(currentKey, newDirectionKey, 1000);
     }
 
     const forwardPoint = getPointAtOffset(currentPoint, currentFacingDirection);
@@ -94,36 +95,85 @@ const solve = (input: string): number => {
     }
   }
 
-  const exitKey = pointToKey(exitPoint);
-  const shortestPath = dijkstra.shortestPath(startKey, (key) =>
-    key.startsWith(exitKey)
-  );
-  return shortestPath;
+  const isExitKey = (key: string) => pointEq(fromKey(key)[0], exitPoint);
+  const shortestPathResult = dijkstra.shortestPath(startKey, isExitKey);
+
+  if (returnCost) {
+    return shortestPathResult.cost;
+  } else {
+    // Keep track of all points in each shortest path, then count them up
+    const allShortestPaths: Set<string>[] = [];
+
+    // DFS from end to start
+    const path: string[] = [];
+
+    // Exit points may be accessible from multiple directions, so find all of them
+    const stack: string[] = [];
+    for (const key of shortestPathResult.prev.keys()) {
+      if (isExitKey(key)) {
+        stack.push(key);
+      }
+    }
+
+    while (stack.length > 0) {
+      const currentKey = stack.pop()!;
+      path.push(currentKey);
+
+      if (currentKey === startKey) {
+        allShortestPaths.push(new Set(path));
+      }
+
+      const parents = shortestPathResult.prev.get(currentKey);
+
+      if (!parents || parents.size === 0) {
+        path.pop();
+      } else {
+        stack.push(...Array.from(parents));
+      }
+    }
+
+    const allVertices = allShortestPaths.reduce(
+      (acc, curr) => (acc = union(acc, curr)),
+      new Set()
+    );
+    const allPointKeys = Array.from(allVertices).map((key) =>
+      pointToKey(fromKey(key)[0])
+    );
+    const uniqueKeys = new Set(allPointKeys);
+    console.log(`uniqueKeys: ${JSON.stringify(Array.from(uniqueKeys))}`);
+    return uniqueKeys.size;
+  }
 };
 
 describe("aoc", () => {
-  test("demo 1", () => {
+  test("part 1, demo 1", () => {
     const input = readToString(path.join(__dirname, "input-demo-1.txt"));
     expect(solve(input)).toEqual(7036);
   });
 
-  test("demo 2", () => {
+  test("part 1, demo 2", () => {
     const input = readToString(path.join(__dirname, "input-demo-2.txt"));
     expect(solve(input)).toEqual(11048);
   });
 
   test("part 1", () => {
     const input = readToString(path.join(__dirname, "input.txt"));
-    expect(solve(input)).toEqual(-1);
+    expect(solve(input)).toEqual(111480);
   });
 
-  test.skip("demo 2", () => {
-    const input = readToString(path.join(__dirname, "input-demo.txt"));
-    expect(solve(input)).toEqual(-1);
+  test("part 2, demo 1", () => {
+    const input = readToString(path.join(__dirname, "input-demo-1.txt"));
+    expect(solve(input, false)).toEqual(45);
   });
 
-  test.skip("part 2", () => {
+  test("part 2, demo 2", () => {
+    const input = readToString(path.join(__dirname, "input-demo-2.txt"));
+    expect(solve(input, false)).toEqual(64);
+  });
+
+  test("part 2", () => {
     const input = readToString(path.join(__dirname, "input.txt"));
-    expect(solve(input)).toEqual(-1);
+    // Solution returns 555, which is too high
+    expect(solve(input, false)).toEqual(-1);
   });
 });
--- packages/aoc-utils/src/algorithms/dijkstra.ts
+++ packages/aoc-utils/src/algorithms/dijkstra.ts
@@ -3,9 +3,40 @@ import {
   MinPriorityQueue,
 } from "@datastructures-js/priority-queue";
 
+export interface ShortestPathResult {
+  /** Total cost of the shortest path */
+  cost: number;
+
+  /** A structure holding all possible vertices leading to destination */
+  prev: Map<string, Set<string>>;
+}
+
 /**
  * An implementation of Dijkstra's algorithm using a priority queue. Vertices
  * must be uniquely identifiable by a string key.
+ *
+ * The `shortestPath` method returns path elements, not just a cost. See
+ * **Discussion*.
+ *
+ * **Discussion**
+ *
+ * The `shortestPath` method returns the shortest path in order, not just a cost.
+ *
+ * > A more general problem is to find all the shortest paths between source and
+ * > target (there might be several of the same length). Then instead of storing
+ * > only a single node in each entry of prev[] all nodes satisfying the
+ * > relaxation condition can be stored. For example, if both r and source
+ * > connect to target and they lie on different shortest paths through target
+ * > (because the edge cost is the same in both cases), then both r and source
+ * > are added to prev[target]. When the algorithm completes, prev[] data
+ * > structure describes a graph that is a subset of the original graph with
+ * > some edges removed. Its key property is that if the algorithm was run with
+ * > some starting node, then every path from that node to any other node in the
+ * > new graph is the shortest path between those nodes graph, and all paths of
+ * > that length from the original graph are present in the new graph. Then to
+ * > actually find all these shortest paths between two given nodes, a path
+ * > finding algorithm on the new graph, such as depth-first search would work.
+ * > -- Wikipedia: https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
  */
 export class Dijkstra {
   private readonly graph: Map<string, Map<string, number>>;
@@ -25,54 +56,76 @@ export class Dijkstra {
    * Returns the lowest-cost path from start to end, or `NaN` if no path is
    * found.
    */
-  shortestPath(start: string, isEnd: (v: string) => boolean): number {
+  shortestPath(
+    start: string,
+    isEnd: (v: string) => boolean
+  ): ShortestPathResult {
     const distances = new Map<string, number>();
     const pq = new MinPriorityQueue<PriorityQueueElement>(pqComparator);
     const visited = new Set<string>();
 
+    const prev = new Map<string, Set<string>>();
     distances.set(start, 0);
-    pq.enqueue([start, 0]);
+    pq.enqueue({
+      key: start,
+      weight: 0,
+    });
 
     while (!pq.isEmpty()) {
-      const current = pq.dequeue()[0];
+      const currentElement = pq.dequeue();
 
-      if (isEnd(current)) {
-        return distances.get(current) ?? 0;
-      }
-
-      if (visited.has(current)) {
+      if (visited.has(currentElement.key)) {
         continue;
       }
 
-      visited.add(current);
+      visited.add(currentElement.key);
 
-      const neighbors = this.graph.get(current);
+      if (isEnd(currentElement.key)) {
+        const result = {
+          cost: distances.get(currentElement.key) ?? 0,
+          prev,
+        };
+        return result;
+      }
+
+      const neighbors = this.graph.get(currentElement.key);
       if (!neighbors) {
         continue;
       }
 
       for (const [neighbor, weight] of neighbors.entries()) {
-        const distance = (distances.get(current) ?? 0) + weight;
+        const distance = (distances.get(currentElement.key) ?? 0) + weight;
 
         if (
           !distances.has(neighbor) ||
-          distance < (distances.get(neighbor) ?? 0)
+          distance <= (distances.get(neighbor) ?? 0)
         ) {
           distances.set(neighbor, distance);
-          pq.enqueue([neighbor, distance]);
+          if (!prev.has(neighbor)) {
+            prev.set(neighbor, new Set<string>());
+          }
+          prev.get(neighbor)!.add(currentElement.key);
+
+          pq.enqueue({
+            key: neighbor,
+            weight: distance,
+          });
         }
       }
     }
 
-    return NaN;
+    return {
+      cost: NaN,
+      prev,
+    };
   }
 }
 
-/**
- * Priority queue elements are a tuple of [weight, id]
- */
-type PriorityQueueElement = [string, number];
+interface PriorityQueueElement {
+  key: string;
+  weight: number;
+}
 
 const pqComparator: IGetCompareValue<PriorityQueueElement> = (
   a: PriorityQueueElement
-): number => a[1];
+): number => a.weight;
