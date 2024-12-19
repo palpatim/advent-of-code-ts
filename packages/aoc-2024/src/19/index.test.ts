d024efffa409d026c646d146fc1a85de498280f8
Tim Schmelter <schmelte@amazon.com>
2024-12-19 07:26:38 -0800

    Day 19

--- packages/aoc-2024/src/19/index.test.ts
+++ packages/aoc-2024/src/19/index.test.ts
@@ -1,28 +1,69 @@
-import { readToString } from '@palpatim/aoc-utils';
+import { addTwoValues, readToString } from "@palpatim/aoc-utils";
 import * as path from "node:path";
 
-const solve = (input: string): number => {
-  return -1;
+const countCombinations = (
+  target: string,
+  inventory: Set<string>,
+  memo: Map<string, number>
+): number => {
+  if (target.length === 0) {
+    return 1;
+  }
+
+  if (memo.has(target)) {
+    return memo.get(target)!;
+  }
+
+  let count = 0;
+  for (const frag of inventory) {
+    if (target.startsWith(frag)) {
+      count += countCombinations(target.slice(frag.length), inventory, memo);
+    }
+  }
+
+  memo.set(target, count);
+  return count;
+};
+
+const solve = (input: string): Record<string, number> => {
+  const [inventoryStr, patternStr] = input.split("\n\n");
+  const inventory = new Set(inventoryStr.split(", "));
+  const patterns = patternStr.split("\n");
+
+  const results: Record<string, number> = {};
+  for (const pattern of patterns) {
+    results[pattern] = countCombinations(
+      pattern,
+      inventory,
+      new Map<string, number>()
+    );
+  }
+
+  return results;
 };
 
 describe("aoc", () => {
-  test("demo 1", () => {
+  test("demo", () => {
     const input = readToString(path.join(__dirname, "input-demo.txt"));
-    expect(solve(input)).toEqual(-1);
+    const results = solve(input);
+    const reachablePatterns = Object.values(results).filter(
+      (r) => r > 0
+    ).length;
+    const totalCombinations = Object.values(results).reduce(addTwoValues, 0);
+
+    expect(reachablePatterns).toEqual(6);
+    expect(totalCombinations).toEqual(16);
   });
 
-  test("part 1", () => {
+  test("real", () => {
     const input = readToString(path.join(__dirname, "input.txt"));
-    expect(solve(input)).toEqual(-1);
-  });
+    const results = solve(input);
+    const reachablePatterns = Object.values(results).filter(
+      (r) => r > 0
+    ).length;
+    const totalCombinations = Object.values(results).reduce(addTwoValues, 0);
 
-  test("demo 2", () => {
-    const input = readToString(path.join(__dirname, "input-demo.txt"));
-    expect(solve(input)).toEqual(-1);
-  });
-
-  test("part 2", () => {
-    const input = readToString(path.join(__dirname, "input.txt"));
-    expect(solve(input)).toEqual(-1);
+    expect(reachablePatterns).toEqual(213);
+    expect(totalCombinations).toEqual(1016700771200474);
   });
 });
