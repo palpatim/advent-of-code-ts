dd8d06f226d1b169e1d669baa4e29797538e6895
Tim Schmelter <schmelte@amazon.com>
2025-12-07 07:43:12 -0800

    2025, Day 07

--- packages/aoc-2025/src/07/index.test.ts
+++ packages/aoc-2025/src/07/index.test.ts
@@ -1,28 +1,124 @@
-import { readToString } from '@palpatim/aoc-utils';
+import {
+  getCellAtPoint,
+  getPointAtOffset,
+  Grid,
+  GridIterator,
+  isValidPoint,
+  Point,
+  pointToKey,
+  readToString,
+} from "@palpatim/aoc-utils";
 import * as path from "node:path";
 
-const solve = (input: string): number => {
-  return -1;
+type GridElement = "." | "S" | "|" | "^";
+
+const countSplits = (input: string): number => {
+  const grid: Grid<GridElement> = input
+    .split("\n")
+    .map((line) => line.split("") as GridElement[]);
+
+  const iter = new GridIterator(grid);
+
+  // Find start point
+  const start = iter.findResult((v) => v.value === "S");
+  if (!start) return -1;
+
+  const queue: Point[] = [start.point];
+  const visited: Set<string> = new Set();
+  let splitCount = 0;
+
+  while (queue.length > 0) {
+    const curr = queue.shift()!;
+
+    const key = pointToKey(curr);
+    if (visited.has(key)) {
+      continue;
+    }
+    visited.add(key);
+
+    const value = getCellAtPoint(curr, grid);
+
+    if (value === "^") {
+      splitCount += 1;
+      const left = getPointAtOffset(curr, "W");
+      if (isValidPoint(left, grid) && !visited.has(pointToKey(left))) {
+        queue.push(left);
+      }
+      const right = getPointAtOffset(curr, "E");
+      if (isValidPoint(right, grid) && !visited.has(pointToKey(right))) {
+        queue.push(right);
+      }
+    } else {
+      const next = getPointAtOffset(curr, "S");
+      if (isValidPoint(next, grid) && !visited.has(pointToKey(next))) {
+        queue.push(next);
+      }
+    }
+  }
+
+  return splitCount;
+};
+
+const countPaths = (input: string): number => {
+  const grid: Grid<GridElement> = input
+    .split("\n")
+    .map((line) => line.split("") as GridElement[]);
+
+  const iter = new GridIterator(grid);
+
+  // Find start point
+  const start = iter.findResult((v) => v.value === "S");
+  if (!start) {
+    return -1;
+  }
+
+  const countsByPoint: Record<string, number> = {};
+
+  const countPathsImpl = (point: Point, grid: Grid<GridElement>): number => {
+    if (countsByPoint[pointToKey(point)]) {
+      return countsByPoint[pointToKey(point)];
+    }
+
+    // We've reached the bottom of the grid, return 1 for the path count
+    if (!isValidPoint(point, grid)) {
+      return 1;
+    }
+
+    const value = getCellAtPoint(point, grid);
+    let count: number;
+    if (value === "^") {
+      const left = getPointAtOffset(point, "W");
+      const right = getPointAtOffset(point, "E");
+      count = countPathsImpl(left, grid) + countPathsImpl(right, grid);
+    } else {
+      const next = getPointAtOffset(point, "S");
+      count = countPathsImpl(next, grid);
+    }
+    countsByPoint[pointToKey(point)] = count;
+    return count;
+  };
+
+  return countPathsImpl(start.point, grid);
 };
 
 describe("aoc", () => {
   test("demo 1", () => {
     const input = readToString(path.join(__dirname, "input-demo.txt"));
-    expect(solve(input)).toEqual(-1);
+    expect(countSplits(input)).toEqual(21);
   });
 
   test("part 1", () => {
     const input = readToString(path.join(__dirname, "input.txt"));
-    expect(solve(input)).toEqual(-1);
+    expect(countSplits(input)).toEqual(1546);
   });
 
   test("demo 2", () => {
     const input = readToString(path.join(__dirname, "input-demo.txt"));
-    expect(solve(input)).toEqual(-1);
+    expect(countPaths(input)).toEqual(40);
   });
 
   test("part 2", () => {
     const input = readToString(path.join(__dirname, "input.txt"));
-    expect(solve(input)).toEqual(-1);
+    expect(countPaths(input)).toEqual(13883459503480);
   });
 });
